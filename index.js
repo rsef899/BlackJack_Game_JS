ba7a37f84dc737246854b97307fbdd27a76374d3
Robert Sefaj <rsef899@aucklanduni.ac.nz>
2022-07-15 23:20:06 +1200

    Created start game and new Game buttons and there functions

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-let firstCard = 10
-let lastCard = 10
+let firstCard;
+let lastCard;
+let sum;
 let hasBlackJacked = false
+let firstPlayed = false;
+
+let messageEl = document.getElementById("message-el")
+
+//--------selecting the sum element
+//let sumEl = document.getElementById("sum-el")
+//same as by element but this searches the code for a selector,**Class
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function addCard(){
+    lastCard = 5
+    sum += lastCard
+    startGame()
+}
 
 function startGame(){
-    let sum = firstCard + lastCard
+    if (firstPlayed == false){
+        firstCard = 12;
+        lastCard = 8;
+        cardsEl.innerText += " " + firstCard;
+        sum = firstCard + lastCard
+        firstPlayed = true;
+    }
+
+    cardsEl.innerText += " " + lastCard
     if (sum === 21){
         console.log("Blackjacked")
         hasBlackJacked = true
     }
     else if (sum < 21){
         console.log("Next Hand")
+        sumEl.textContent = "Sum: " + sum 
     }
     else{
         console.log("Busted")
+        sumEl.textContent = "Sum: " + sum 
     }
     
 }
