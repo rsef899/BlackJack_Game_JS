a1793cb11f373068561bd546266d46fc851a78bc
Robert Sefaj <rsef899@aucklanduni.ac.nz>
2022-07-15 21:04:55 +1200

    Revert "Settign up initial Pages"
    This reverts commit cc1245313a6e92e3a8c8aa93d0c22796bbaef33a.
    
    Initial File setup

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-let firstcard = Math.floor(Math.random() * 18) + 2
-let lastcard = Math.floor(Math.random() * 18) + 2
-let sum = hasBlackJacked = false
-let message = ""
-
-if ((firstcard + lastcard) < 21){
-    message = 'do you want to draw a new card'
-}
-else if ((firstcard + lastcard) === 21){
-    message = "you got black jack"
-    console.log(message)
-    hasBlackJaceked = true
-}
-else {
-    message = "your out of the game"
-    console.log(message)
-}
+let firstcard = math.floor(Math.random() * 11) + 2
