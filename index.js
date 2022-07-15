77abef71f8a29e3853530bb7e5824c76bb8e3418
Robert Sefaj <rsef899@aucklanduni.ac.nz>
2022-07-15 21:48:01 +1200

    Framework for the blackJack game setup, imporved css

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-let firstcard = math.floor(Math.random() * 11) + 2
+let firstCard = 10
+let lastCard = 10
+let hasBlackJacked = false
+
+function startGame(){
+    let sum = firstCard + lastCard
+    if (sum === 21){
+        console.log("Blackjacked")
+        hasBlackJacked = true
+    }
+    else if (sum < 21){
+        console.log("Next Hand")
+    }
+    else{
+        console.log("Busted")
+    }
+    
+}
