31ff809af24047080ac4205855b0c2068b986465
Robert Sefaj <rsef899@aucklanduni.ac.nz>
2022-07-16 14:09:53 +1200

    Added random card generation and reset after black jack

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let firstCard;
 let lastCard;
-let sum;
+let sum = 0;
 let hasBlackJacked = false
-let firstPlayed = false;
+let isAlive = true
+//array 
+let cards =[];
 
 let messageEl = document.getElementById("message-el")
 
@@ -12,25 +14,36 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-function addCard(){
-    lastCard = 5
-    sum += lastCard
-    startGame()
+/**
+ * getting a random number for the cards
+ */
+function getRandomCard(){
+    let randomvalue = Math.floor(Math.random() * 12) + 1
+    if (randomvalue === 1){
+        return 11
+    }
+    else if (randomvalue > 10){
+        return 10
+    }
+    else{
+        return randomvalue
+    }
 }
 
-function startGame(){
-    if (firstPlayed == false){
-        firstCard = 12;
-        lastCard = 8;
-        cardsEl.innerText += " " + firstCard;
-        sum = firstCard + lastCard
-        firstPlayed = true;
-    }
+function addCard(){
+    lastCard = getRandomCard()
+    cards.push(lastCard)
+    sum += lastCard
+    playGame()
+}
 
+
+function playGame(){
     cardsEl.innerText += " " + lastCard
     if (sum === 21){
         console.log("Blackjacked")
         hasBlackJacked = true
+        isAlive = false
     }
     else if (sum < 21){
         console.log("Next Hand")
@@ -39,6 +52,25 @@ function startGame(){
     else{
         console.log("Busted")
         sumEl.textContent = "Sum: " + sum 
+        isAlive = false
+    }
+    if (!isAlive){
+        cards = []
+        console.log(cards)
+        cardsEl.textContent = "Cards: "
+        sum = 0
+        sumEl.textContent = "Sum: "
+        isAlive = true
     }
     
+}
+//only used when the start game button is clocked
+function startGame(){
+    firstCard = getRandomCard();
+    lastCard = getRandomCard();
+    cards.push(firstCard)
+    cards.push(lastCard)
+    cardsEl.innerText += " " + firstCard;
+    sum = cards[0] + cards[1]
+    playGame()
 }
