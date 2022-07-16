9f0b3ff8d2c37477ca7d4be7f1f80177953ede35
Robert Sefaj <rsef899@aucklanduni.ac.nz>
2022-07-16 14:37:05 +1200

    dom manipulaiton to make the start game and the  new card button dissapear at certain moments

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let messageEl = document.getElementById("message-el")
 //same as by element but this searches the code for a selector,**Class
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
+document.getElementById("newCardButton").style.visibility = "hidden"
 
 /**
  * getting a random number for the cards
@@ -39,29 +40,35 @@ function addCard(){
 
 
 function playGame(){
+    document.getElementById("result-el").textContent = ""
     cardsEl.innerText += " " + lastCard
     if (sum === 21){
         console.log("Blackjacked")
         hasBlackJacked = true
+        document.getElementById("result-el").textContent = " You Blackjacked!, play again"
         isAlive = false
     }
     else if (sum < 21){
         console.log("Next Hand")
         sumEl.textContent = "Sum: " + sum 
+        document.getElementById("result-el").textContent = " pickup a new card"
     }
     else{
         console.log("Busted")
         sumEl.textContent = "Sum: " + sum 
+        document.getElementById("result-el").textContent = "You Busted Play A new game"
         isAlive = false
     }
     if (!isAlive){
         cards = []
-        console.log(cards)
         cardsEl.textContent = "Cards: "
         sum = 0
         sumEl.textContent = "Sum: "
         isAlive = true
+        document.getElementById("startButton").removeAttribute("style")
+        document.getElementById("newCardButton").style.visibility = "hidden"
     }
+    
     
 }
 //only used when the start game button is clocked
@@ -73,4 +80,11 @@ function startGame(){
     cardsEl.innerText += " " + firstCard;
     sum = cards[0] + cards[1]
     playGame()
+
+    //removing the start game button when the game has started adn mocign the new card button up
+    document.getElementById("startButton").style.visibility = "hidden"
+    document.getElementById("startButton").style.bottom = "1000px"
+    document.getElementById("newCardButton").style.visibility = "visible"
+    document.getElementById("newCardButton").style.top = "6px"
+   
 }
